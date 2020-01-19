603ef6d90ed315d5cbce4b4b4710e645b334c3fc
Basoro <dentix.id@gmail.com>
2020-01-19 08:56:34 +0800

    Update

--- static/inc/js/scripts.js
+++ static/inc/js/scripts.js
@@ -39,11 +39,11 @@ $(document).on('click touchstart', '[data-confirm]:not(.disabled):not([disabled]
                 if(source.is('[type="submit"]'))
                 {
                     $(document).off('click touchstart', '[data-confirm]:not(.disabled):not([disabled])');
-                    source.click(); 
+                    source.click();
                 }
                 else if(source.is('a'))
                 {
-                    $(location).attr('href', source.attr('href'));    
+                    $(location).attr('href', source.attr('href'));
                 }
             }
         }
@@ -74,29 +74,6 @@ $(function () {
 	}
 });
 
-/* 6. SORTABLE SIDEBAR
---------------------------------------------------------- */
-$(function () {
-    sortable('.sidebar-nav', {handle:'i'})[0].addEventListener('sortupdate', function(e) {
-        var baseURL = batflat.url + '/' + batflat.admin;
-        var items   = {};
-
-        $(e.detail.endparent).children('li').each(function(index, element) {
-            var module = $(element).data('module');
-            items[module] = index;
-        });
-
-        $.ajax({
-            url: baseURL + '/settings/changeOrderOfNavItem?t=' + batflat.token,
-            type: 'POST',
-            cache: false,
-            data: items,
-            success: function(respond) {
-                console.log(respond);
-            }
-        });
-    });
-});
 
 /* 7. TINYNAV
 --------------------------------------------------------- */
@@ -104,44 +81,4 @@ $(function () {
     $('.panel-heading .nav-tabs').tinyNav({
         active: 'active'
     });
-});
-
-/* 8. CUSTOM CHECKBOXES & RADIO BUTTONS
---------------------------------------------------------- */
-$(':checkbox').kalypto();
-$(':radio').kalypto({toggleClass: 'toggleR'});
-
-/* 9. REMOTE MODAL
---------------------------------------------------------- */
-$('a[data-toggle="modal"]').on('click', function(e) {
-    var target_modal = $(e.currentTarget).data('target');
-    var remote_content = $(e.currentTarget).attr('href');
-
-    if(remote_content.indexOf('#') === 0) return; 
-
-    var modal = $(target_modal);
-    var modalContent = $(target_modal + ' .modal-content');
-
-    modal.off('show.bs.modal');
-    modal.on('show.bs.modal', function () {
-        modalContent.load(remote_content);
-    }).modal();
-        
-    return false;
-});
-
-/* 10. CUSTOM SELECT
---------------------------------------------------------- */
-$('select').each(function () {
-    var options = {
-        useDimmer: true,
-        useSearch: false,
-        labels: {
-            search: '...'
-        }
-    };
-    $.each($(this).data(), function (key, value) {
-        options[key] = value;
-    });
-    $(this).selectator(options);
 });
